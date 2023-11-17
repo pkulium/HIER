1daad36b2876501c355148b8c8cf92c34ee048c3
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-17 11:27:45 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   options.py

--- options.py
+++ options.py
@@ -38,7 +38,7 @@ def args_parser():
     parser.add_argument(
         '--num_communication',
         type = int,
-        default=50,
+        default=100,
         help = 'number of communication rounds with the cloud server'
     )
     parser.add_argument(
