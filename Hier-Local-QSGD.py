a76a0c4128e133a03ceda54b94a35c4ea08c22fd
pkulium <54947136+pkulium@users.noreply.github.com>
2023-12-31 13:34:44 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Hier-Local-QSGD.py

--- Hier-Local-QSGD.py
+++ Hier-Local-QSGD.py
@@ -83,7 +83,7 @@ def fast_all_clients_test_attack(v_test_loader, global_nn, device, args):
         with torch.no_grad():
             for data in v_test_loader:
                 inputs, labels = data
-                inputs[:, 0, 0:3, 0:3] = 255
+                inputs[:, 0, 0:2, 0:2] = 255
                 inputs = Variable(inputs).to(device)
                 labels = Variable(labels).to(device)
                 outputs = global_nn(inputs)
@@ -256,7 +256,7 @@ def Hier_Local_QSGD(args):
             elif args.attack == 'backdoor_attack':
                 for idx in clients[i].train_loader.dataset.idxs:
                     clients[i].train_loader.dataset.dataset.targets[idx] = 7
-                    clients[i].train_loader.dataset.dataset.data[idx, 0:3, 0:3] = 255
+                    clients[i].train_loader.dataset.dataset.data[idx, 0:2, 0:2] = 255
             else:
                 if args.attack == 'coordinate_attack0':
                     clients[i].train_loader.dataset.idxs = clients[i].train_loader.dataset.idxs 
